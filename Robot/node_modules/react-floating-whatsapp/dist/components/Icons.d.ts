cc3000bcce46dc82047719dabbdc3e1f80a37794
AliKazmi <ali.kazmi@student.hpi.uni-potsdam.de>
2023-06-13 10:58:16 +0200

    Deleted Unused files

--- Robot/node_modules/react-floating-whatsapp/dist/components/Icons.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-export declare function WhatsappSVG(): JSX.Element;
-export declare function CheckSVG(): JSX.Element;
-export declare function CloseSVG(): JSX.Element;
-export declare function SendSVG(): JSX.Element;
